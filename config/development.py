6d4ee8ef6808ad3842a2905151b6f2efb2316f6c
Kyeongwon <aelleek@gmail.com>
2021-01-27 15:54:41 +0900

    Add Database config info

--- config/development.py
+++ config/development.py
@@ -4,3 +4,15 @@
 
   Created by LeeKW on 2021/01/27.
 """
+
+# from config.default import *
+
+
+DB_NAME = "sprinkle"
+DB_ID = "sprinkle"
+DB_PWD = "bitbitr35"
+# DB_IP
+# DB_PORT
+
+MONGO_DATABASE_URI = 'mongodb://{DB_ID}:{DB_PWD}@localhost:27017/{DB_NAME}'
+SECRET_KEY = "dev"

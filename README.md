0b573c552309dfd9417e2062b834243501671df1
Kyeongwon <aelleek@gmail.com>
2021-01-27 15:45:00 +0900

    Add DB Setting and Test Mongodb Connection

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # sprinkle
 Team Sprinkle Secretary Program Project
+
+
+
+
+
+mongodb-community@4.2 is keg-only, which means it was not symlinked into /usr/local,
+because this is an alternate version of another formula.
+
+If you need to have mongodb-community@4.2 first in your PATH run:
+  echo 'export PATH="/usr/local/opt/mongodb-community@4.2/bin:$PATH"' >> ~/.zshrc
+
+
+To have launchd start mongodb/brew/mongodb-community@4.2 now and restart at login:
+  brew services start mongodb/brew/mongodb-community@4.2
+Or, if you don't want/need a background service you can just run:
+  mongod --config /usr/local/etc/mongod.conf
--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@
 """
 
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -13,7 +14,20 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route('/users')
+def getUsers():
+    client = MongoClient('mongodb://sprinkle:bitbitr35@localhost:27017/sprinkle')
+    data = client.sprinkle.member.find()
+    for i in data:
+          print(i)
+    return "성공"
+
+
 if __name__ == '__main__':
     # kochat.app.template_folder = kochat.root_dir + 'templates'
     # kochat.app.static_folder = kochat.root_dir + 'static'
+    # app.config["MONGO_URI"] = "mongodb://sprinkle:bitbitr35@localhost:27017/sprinkle"
+    
+    # mongo = PyMongo(app)
     app.run(port=8080, host='0.0.0.0')

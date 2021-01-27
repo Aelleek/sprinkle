796f2083f4e2aa085e71a4946e37d9fbabd4a0a4
Kyeongwon <aelleek@gmail.com>
2021-01-27 18:16:25 +0900

    Test config file

--- app/app.py
+++ app/app.py
@@ -4,11 +4,21 @@
 
   Created by LeeKW on 2021/01/26.
 """
+# -*- coding: utf-8 -*- 
 
 from flask import Flask
 from pymongo import MongoClient
 
+import sys
+import config
+
+
 app = Flask(__name__)
+
+# app.config.from_envvar('APP_CONFIG_FILE')
+
+# ‘환경 변수 APP_CONFIG_FILE에 정의된 파일을 환경 파일로 사용하겠다’는 의미이다.
+
 
 @app.route('/')
 def hello():
@@ -17,11 +27,12 @@ def hello():
 
 @app.route('/users')
 def getUsers():
-    client = MongoClient('mongodb://sprinkle:bitbitr35@localhost:27017/sprinkle')
+    client = MongoClient(app.config.MONGO_DATABASE_URI)
     data = client.sprinkle.member.find()
     for i in data:
           print(i)
     return "성공"
+
 
 
 if __name__ == '__main__':
@@ -30,4 +41,20 @@ if __name__ == '__main__':
     # app.config["MONGO_URI"] = "mongodb://sprinkle:bitbitr35@localhost:27017/sprinkle"
     
     # mongo = PyMongo(app)
+
+    env = sys.argv[1] if len(sys.argv) > 2 else 'dev'
+    
+    if env == 'dev':
+        app.config = config.DevelopmentConfig
+    elif env == 'test':
+        app.config = config.TestConfig
+    elif env == 'prod':
+        app.config = config.ProductionConfig
+    else:
+        raise ValueError('Invalid environment name')
+   
+
     app.run(port=8080, host='0.0.0.0')
+
+
+    # https://mingrammer.com/ways-to-manage-the-configuration-in-python/
--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-  __init__.py
-  Sprinkle
-
-  Created by LeeKW on 2021/01/27.
-"""
